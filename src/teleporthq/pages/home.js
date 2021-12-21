3d10fa0d7cbc1b63ce6666ba2a1a4f311f1e8d7a
Pașca Ionuț <pascaionut94@gmail.com>
2021-12-21 16:36:19 +0200

    Commit from TeleportHQ

---
+++ src/teleporthq/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import styles from './home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles['container']}>
+      <Helmet>
+        <title>New Project</title>
+        <meta property="og:title" content="New Project" />
+      </Helmet>
+      <div className={styles['container1']}></div>
+    </div>
+  )
+}
+
+export default Home
